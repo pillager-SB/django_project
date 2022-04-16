f2660be3a3395ce480c5aa861962a43917b86897
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-04-16 19:40:00 +0300

    step_03

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp import views as mainapp
 
 urlpatterns = [
+    path('', mainapp.index),
+    path('products/', mainapp.products),
+    path('contact/', mainapp.contact),
     path('admin/', admin.site.urls),
 ]
