db978caa60cc939463cb3c80ae1e406ef013f01c
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-04-23 08:32:30 +0300

    Lesson_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse
 
+MENU_LINKS = {
+    'index': 'Главная',
+    'products': 'Товары',
+    'contact': 'Контакты',
+}
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={
+        'title': 'Магазин: Главная',
+        'menu': MENU_LINKS,
+    })
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context={
+        'title': 'Магазин: Товары',
+        'menu': MENU_LINKS,
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context={
+        'title': 'Магазин: Контакты',
+        'menu': MENU_LINKS,
+    })
