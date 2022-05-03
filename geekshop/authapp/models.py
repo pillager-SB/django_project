824b3d65f0277fadc93418487574a97d23d30279
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-05-03 17:41:28 +0300

    new branch

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name='возраст')
+    age = models.PositiveIntegerField(verbose_name='возраст', null=True)
+    city = models.CharField(max_length=64, blank=True)
